6ec95b6586737adfa0d80f6c69936ac4b5eb9e6f
Mitch Lee <mitchlee11@gmail.com>
2016-06-22 12:41:38 -0700

    Add support for WKWebView.

--- src/ios/screenshot.m
+++ src/ios/screenshot.m
@@ -14,10 +14,14 @@
                        queue:mainQueue
                   usingBlock:^(NSNotification *note) {
                     if ([self.webView respondsToSelector:@selector(stringByEvaluatingJavaScriptFromString:)]) {
+                        // UIWebView
                         [self.webView performSelectorOnMainThread:@selector(stringByEvaluatingJavaScriptFromString:) withObject:@"cordova.fireDocumentEvent('screenshot');" waitUntilDone:NO];
+                    } else if ([self.webView respondsToSelector:@selector(evaluateJavaScript:completionHandler:)]) {
+                        // WKWebView
+                        [self.webView performSelector:@selector(evaluateJavaScript:completionHandler:) withObject:@"cordova.fireDocumentEvent('screenshot');" withObject:nil];
                     } else {
                         // cordova lib version is > 4
-                        [self.webView performSelectorOnMainThread:@selector(evaluateJavaScript:completionHandler:) withObject:@"cordova.fireDocumentEvent('screenshot');"  waitUntilDone:NO];
+                        [self.webView performSelectorOnMainThread:@selector(evaluateJavaScript:completionHandler:) withObject:@"cordova.fireDocumentEvent('screenshot');" waitUntilDone:NO];
                     }
                   }];
     }
